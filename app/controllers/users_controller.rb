d16918ae3d8d9a6da53d1a4ec9d327ef37b9ac42
danialzainul <danialzainul@hotmail.com>
2017-04-27 00:10:25 +0800

    Added search and add friend to user_controller and ajax friend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,26 @@ class UsersController < ApplicationController
   def my_friends
     @friendships = current_user.friends
   end
+
+  def search
+    @users = User.search(params[:search_params])
+
+    if @users
+      @users = current_user.except_current_user(@users) #so that current_user does not show up on the search
+      render partial: 'friends/lookup'
+    else
+      render status: :not_found, nothing: true
+    end
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+
+    if current_user.save
+      redirect_to my_friends_path, notice: "Friend was successfully added"
+    else
+      redirect_to my_friends_path, flash[:error] = "Unable to add friend. Please try again"
+    end
+  end
 end
