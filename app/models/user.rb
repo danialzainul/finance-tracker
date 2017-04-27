135e67a3e98d803cfb5e7d66ff160059e5becd9c
danialzainul <danialzainul@hotmail.com>
2017-04-28 01:21:23 +0800

    Added destroy friendship and show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
   has_many :friends, through: :friendships
 
   def full_name
-    return "#{first_name} #{last_name}.".strip if (first_name || last_name)
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
     "Anonymous"
   end
 
